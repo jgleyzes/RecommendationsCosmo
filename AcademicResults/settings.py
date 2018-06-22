b96e7d5e064628c2936b03a7ca2afc6033333acb
jgleyzes <j.gleyzes@gmail.com>
2018-06-22 12:49:41 -0700

    version with graphs

--- AcademicResults/settings.py
+++ AcademicResults/settings.py
@@ -24,7 +24,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = '#j@%nvjuphh&=g0c+ihmlt5$kj__7cml=vp-7sg9-4r_$--hjn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,8 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'django_elasticsearch_dsl',
     'app_compare',
 ]
+
+# ELASTICSEARCH_DSL={
+#     'default': {
+#         'hosts': 'localhost:9200'
+#     },
+# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
